56da5eaf85c04de6a3f3bfa21baecaeef9c08a0d
eddcaton <42009843+eddcaton@users.noreply.github.com>
2025-12-31 11:06:08 +0000

    Simplify NeoMatrix initialization with host only

--- custom_components/pulse_eight_neo/__init__.py
+++ custom_components/pulse_eight_neo/__init__.py
@@ -2,10 +2,6 @@ from .const import DOMAIN
 from .neo_api import NeoMatrix
 
 async def async_setup_entry(hass, entry):
-    hass.data[DOMAIN] = NeoMatrix(
-        entry.data["host"],
-        entry.data["port"]
-    )
-
+    hass.data[DOMAIN] = NeoMatrix(entry.data["host"])
     await hass.config_entries.async_forward_entry_setups(entry, ["select"])
     return True
