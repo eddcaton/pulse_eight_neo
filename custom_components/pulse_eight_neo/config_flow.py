0695d01c8243faabaff14f0bf1b6c2faf5cd9427
eddcaton <42009843+eddcaton@users.noreply.github.com>
2025-12-31 10:52:00 +0000

    Update config flow to include host in entry title

--- custom_components/pulse_eight_neo/config_flow.py
+++ custom_components/pulse_eight_neo/config_flow.py
@@ -3,15 +3,20 @@ import voluptuous as vol
 from .const import *
 
 class NeoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title="Pulse-Eight Neo", data=user_input)
+            return self.async_create_entry(
+                title=f"Pulse-Eight Neo ({user_input[CONF_HOST]})",
+                data=user_input
+            )
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required(CONF_HOST): str,
-                vol.Optional(CONF_PORT, default=DEFAULT_PORT): int,
-                vol.Optional(CONF_OUTPUTS, default=DEFAULT_OUTPUTS): int
+                vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
+                vol.Required(CONF_OUTPUTS, default=DEFAULT_OUTPUTS): int
             })
         )
