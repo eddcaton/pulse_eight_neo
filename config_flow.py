b5073dca12c564979340a22efc8529c1cbd483c6
eddcaton <42009843+eddcaton@users.noreply.github.com>
2025-12-31 10:17:40 +0000

    Create config_flow.py

---
+++ config_flow.py
@@ -0,0 +1,17 @@
+from homeassistant import config_entries
+import voluptuous as vol
+from .const import *
+
+class NeoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="Pulse-Eight Neo", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(CONF_HOST): str,
+                vol.Optional(CONF_PORT, default=DEFAULT_PORT): int,
+                vol.Optional(CONF_OUTPUTS, default=DEFAULT_OUTPUTS): int
+            })
+        )
