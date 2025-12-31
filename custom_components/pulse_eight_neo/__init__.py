85d40d186fdbfaebb250ce3b167cc7f7b19aa47f
eddcaton <42009843+eddcaton@users.noreply.github.com>
2025-12-31 11:33:02 +0000

    Refactor async_setup_entry to use aiohttp session

--- custom_components/pulse_eight_neo/__init__.py
+++ custom_components/pulse_eight_neo/__init__.py
@@ -1,7 +1,21 @@
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+
 from .const import DOMAIN
 from .neo_api import NeoMatrix
 
+PLATFORMS = ["select"]
+
 async def async_setup_entry(hass, entry):
-    hass.data[DOMAIN] = NeoMatrix(entry.data["host"])
-    await hass.config_entries.async_forward_entry_setups(entry, ["select"])
+    host = entry.data["host"]
+    session = async_get_clientsession(hass)
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = NeoMatrix(host, session)
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+async def async_unload_entry(hass, entry):
+    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    hass.data[DOMAIN].pop(entry.entry_id, None)
     return True
