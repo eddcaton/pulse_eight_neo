70cd7a659072cc72fa77d95c2109b17d4f35df7d
eddcaton <42009843+eddcaton@users.noreply.github.com>
2025-12-31 10:52:19 +0000

    Refactor async_setup_entry for output entities

--- custom_components/pulse_eight_neo/select.py
+++ custom_components/pulse_eight_neo/select.py
@@ -2,19 +2,23 @@ from homeassistant.components.select import SelectEntity
 from .const import DOMAIN
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    outputs = entry.data["outputs"]
     api = hass.data[DOMAIN]
+    outputs = entry.data["outputs"]
 
-    async_add_entities([
-        NeoOutputSelect(api, o+1) for o in range(outputs)
-    ])
+    entities = []
+    for output in range(1, outputs + 1):
+        entities.append(NeoOutputSelect(api, output))
+
+    async_add_entities(entities)
+
 
 class NeoOutputSelect(SelectEntity):
     def __init__(self, api, output):
         self.api = api
         self._output = output
         self._attr_name = f"Neo Output {output}"
-        self._attr_options = [f"Input {i}" for i in range(1,7)]
+        self._attr_unique_id = f"neo_output_{output}"
+        self._attr_options = [f"Input {i}" for i in range(1, 7)]
         self._attr_current_option = "Input 1"
 
     async def async_select_option(self, option):
