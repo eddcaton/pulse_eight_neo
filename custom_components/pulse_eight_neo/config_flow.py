606fd533ce973f4c8f7daf95672d82318737dfde
eddcaton <42009843+eddcaton@users.noreply.github.com>
2025-12-31 11:41:55 +0000

    Simplify user input check in config flow

--- custom_components/pulse_eight_neo/config_flow.py
+++ custom_components/pulse_eight_neo/config_flow.py
@@ -1,13 +1,12 @@
 from homeassistant import config_entries
 import voluptuous as vol
-
 from .const import DOMAIN, CONF_HOST, CONF_OUTPUTS, DEFAULT_OUTPUTS
 
 class NeoConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        if user_input is not None:
+        if user_input:
             return self.async_create_entry(
                 title=f"Pulse-Eight Neo ({user_input[CONF_HOST]})",
                 data=user_input
