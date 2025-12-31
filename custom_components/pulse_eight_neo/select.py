829ab64f5fe2058268f5ab9cbfdcc92b1c32e47e
eddcaton <42009843+eddcaton@users.noreply.github.com>
2025-12-31 11:42:16 +0000

    Refactor NeoOutputSelect initialization and method

--- custom_components/pulse_eight_neo/select.py
+++ custom_components/pulse_eight_neo/select.py
@@ -1,16 +1,14 @@
 from homeassistant.components.select import SelectEntity
-
 from .const import DOMAIN, DEFAULT_INPUTS
 
 async def async_setup_entry(hass, entry, async_add_entities):
     api = hass.data[DOMAIN][entry.entry_id]
     outputs = int(entry.data["outputs"])
 
-    entities = [NeoOutputSelect(api, output=i) for i in range(1, outputs + 1)]
-    async_add_entities(entities)
+    async_add_entities([NeoOutputSelect(api, o) for o in range(1, outputs + 1)])
 
 class NeoOutputSelect(SelectEntity):
-    def __init__(self, api, output: int):
+    def __init__(self, api, output):
         self.api = api
         self._output = output
         self._attr_name = f"Neo Output {output}"
@@ -18,11 +16,8 @@ class NeoOutputSelect(SelectEntity):
         self._attr_options = [f"Input {i}" for i in range(1, DEFAULT_INPUTS + 1)]
         self._attr_current_option = "Input 1"
 
-    async def async_select_option(self, option: str) -> None:
+    async def async_select_option(self, option):
         input_num = int(option.split(" ")[1])
-
-        status, body = await self.api.route(output_1based=self._output, input_1based=input_num)
-
-        # Update UI state regardless; if status is None, logs will show why
+        await self.api.route(self._output, input_num)
         self._attr_current_option = option
         self.async_write_ha_state()
